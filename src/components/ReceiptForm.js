0ad13a8970d8c684e2837f1601835573ca3942de
tanjinhossain1 <tanjinhossain2003@gmail.com>
2022-07-28 16:17:12 +0600

    Receipt data show on the table is complete

--- src/components/ReceiptForm.js
+++ src/components/ReceiptForm.js
@@ -2,56 +2,81 @@ import React, { useState } from 'react';
 import './ReceiptForm.css';
 
 const ReceiptForm = () => {
-    const [data, setData] = useState([])
+    const [receipt, setReceipt] = useState([])
     const receiptSubmit = (event) => {
         event.preventDefault()
         const date = event.target.date.value;
         const amount = event.target.amount.value;
-        const allData = { date, amount }
-        setData([...data, allData])
+        const payment = event.target.payment.value;
+        const remark = event.target.remark.value;
+        const allData = { date, amount, payment, remark }
+        setReceipt([...receipt, allData])
         event.target.reset()
     }
-
+    console.log(receipt)
     return (
         <div className='container'>
             <div className='receipt-container'>
                 <h3 className='receipt-details'>Receipt Details </h3>
                 <form className='form-container' onSubmit={receiptSubmit}>
-
+                    {/* date input  */}
                     <div>
                         <label htmlFor="date">Date<sup>*</sup></label>
-                        <input className='dateInput' name='date' type="text" placeholder='Enter Date' />
+                        <input required className='dateInput' name='date' type="date" placeholder='Enter Date' />
                     </div>
-
+                    {/* amount input  */}
                     <div>
                         <label htmlFor="amount">Amount<sup>*</sup></label>
-                        <input className='amountInput' name='amount' type="number" placeholder='Enter Amount (in INR)' />
+                        <input required className='amountInput' name='amount' type="number" placeholder='Enter Amount (in INR)' />
                     </div>
 
+                    {/* payment mood select  */}
+                    <div>
+                        <label htmlFor="payment">Payment mode<sup>*</sup></label>
+                        <select required className='paymentSelect' name="payment" id="cars">
+                            <option value="Cash">Cash</option>
+                            <option value="Google Pay">Google Pay</option>
+                            <option value="Credit Card">Credit Card</option>
+                            <option value="PayTm">PayTm</option>
+                        </select>
+                    </div>
 
-                   <div>
-                    <label htmlFor="payment">Payment mode<sup>*</sup></label>
-                   <select className='paymentSelect' name="payment" id="cars">
-                        <option value="volvo">Cash</option>
-                        <option value="saab">Google Pay</option>
-                        <option value="mercedes">Credit Card</option>
-                        <option value="audi">PayTm</option>
-                    </select>
-                   </div>
+                    {/* remark input  */}
+                    <div>
+                        <label htmlFor="remark">Remark </label>
+                        <input required className='remarkInput' name='remark' type="text" placeholder='Enter remark' />
+                    </div>
 
-             <div>
-                <label htmlFor="remark">Remark </label>
-             <input className='remarkInput' name='amount' type="text" placeholder='Enter remark' />
-             </div>
-
+                    {/* submit button or cancel button  */}
                     <div className='buttons-container'>
-                        <button className='cancelButton' >CANCEL <br /> <span>(ESC)</span></button>
-                        <button  type="submit"  className='submitButton' >SUBMIT <br /><span> (% S)</span></button>
-                       
+                        <button type='button' className='cancelButton' >CANCEL <br /> <span>(ESC)</span></button>
+                        <button type="submit" className='submitButton' value='SUBMIT'>SUBMIT <br /> <span>(% S)</span></button>
+
                     </div>
                 </form>
 
             </div>
+            <table>
+                <tr>
+                    <th>Number</th>
+                    <th>Date</th>
+                    <th>Amount</th>
+                    <th>Payment mode</th>
+                    <th>Remark</th>
+                </tr>
+                {
+                    receipt.map((r, index) =>
+                        <tr key={index + 1}>
+                            <td>{index}</td>
+                            <td>{r.date}</td>
+                            <td> {r.amount}</td>
+                            <td>{r.payment}</td>
+                            <td>{r.remark}</td>
+                        </tr>
+                    )
+                }
+                <button onClick={()=>setReceipt([])} className='reset-button'>reset</button>
+            </table>
         </div>
     );
 };
